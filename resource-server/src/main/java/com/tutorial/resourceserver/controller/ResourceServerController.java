d39a049740c560e468d363aaec6e34c886b0d529
AntonioJ93 <pryet2@gmail.com>
2023-06-03 22:52:42 +0200

    reaource server ok

---
+++ resource-server/src/main/java/com/tutorial/resourceserver/controller/ResourceServerController.java
@@ -0,0 +1,25 @@
+package com.tutorial.resourceserver.controller;
+
+import com.tutorial.resourceserver.dto.MessageDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
+public class ResourceServerController {
+
+    @GetMapping("/user")
+    public ResponseEntity<MessageDto> user (Authentication authentication){
+        return ResponseEntity.ok(new MessageDto("Hola "+authentication.getName() +" roles "+authentication.getAuthorities().toString()));
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<MessageDto> admin (Authentication authentication){
+        return ResponseEntity.ok(new MessageDto("Hola admin "+authentication.getName() +" roles "+authentication.getAuthorities().toString()));
+    }
+}
